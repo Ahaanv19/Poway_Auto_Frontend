4793661fccfbb1788c8c53cd97a76faa3d1ca2ea
Ahaan Vaidyanathan <ahaanvk@gmail.com>
2025-04-11 13:22:54 -0700

    Made Upadte

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:8888";
 } else {
-        pythonURI = "http://127.0.0.1:8888";
+        pythonURI = "https://autonomous.stu.nighthawkcodingsociety.com.stu.nighthawkcodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
