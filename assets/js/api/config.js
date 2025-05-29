88391d3d52c4c9e1d6e564e5fe1d3ad431458c59
Ahaan Vaidyanathan <ahaanvk@gmail.com>
2025-05-29 09:59:11 -0700

    Updated Backend Fetching

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:8888";
 } else {
-        pythonURI = "https://autonomous.stu.nighthawkcodingsociety.com";
+        pythonURI = "https://autonomous.opencodingsociety.com/";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
@@ -12,7 +12,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         javaURI = "http://127.0.0.1:8888"; //rey
 } else {
-        javaURI = "https://autonomous.stu.nighthawkcodingsociety.com";
+        javaURI = "https://autonomous.opencodingsociety.com/";
 }
 
 export const fetchOptions = {
